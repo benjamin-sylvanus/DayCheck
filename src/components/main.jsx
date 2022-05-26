918540233d13ad1ec5d21475d6503da0abcc7394
Benjamin Sylvanus <Sylvanus.b@northeastern.edu>
2022-05-26 18:41:59 -0400

    Just a Simple Diary

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,68 +1,72 @@
 import React from 'react';
-import {Card, Col, Divider} from 'antd';
+import {Card, Col, Divider, Row} from 'antd';
 
 class Main extends React.Component {
     render() {
         return (
-            <div className="site-card-wrapper">
-                <div style={{display: "flex", margin: "2rem", alignItems: "stretch", justifyContent: "space-evenly"}}>
-                    <Col style={{display: "flex"}}>
-                        <Card title="Exercise" bordered={true} style={{borderRadius: "1.5rem", flexGrow: 1}}>
-                            Went to gym and played basketball.
-                        </Card>
-                    </Col>
-                    <Col style={{display: "flex", maxWidth: "20%"}}>
-                        <Card title="Work" bordered={true}
-                              style={{
-                                  borderRadius: "1.5rem",
-                                  flexGrow: 1,
-                                  textAlign: "left",
-                                  alignItems: "center",
-                                  padding: 10
-                              }}>
-                            <p>Spent time on new project for work. Read about matrix translation and rotation.
-                                Doesn't make
-                                sense yet.
-                            </p>
-                            <Divider/>
-                            <p>
-                                Made progress on website. Specifically practiced using an animation library and
-                                worked on
-                                fundamental CSS styling.
-                            </p>
-                            <Divider/>
-                            <div>
-                                Made this as fast as I could
-                            </div>
+            <div style={{
+                display: "flex",
+                margin: "2rem",
+                alignItems: "center",
+                alignContent: "normal",
+                justifyContent: "space-around",
+                flexDirection: "column",
+                rowGap: "2rem"
+            }}>
+                <Col className="row-width-30">
+                    <Card title="Exercise" bordered={true} style={{borderRadius: "1.5rem", flexGrow: 1}}>
+                        Went to gym and played basketball.
+                    </Card>
+                </Col>
+                <Row className="row-width-30">
+                    <Card title="Work" bordered={true}
+                          style={{
+                              borderRadius: "1.5rem",
+                              flexGrow: 1,
+                              textAlign: "left",
+                              padding: 10
+                          }}>
+                        <p>Spent time on new project for work. Read about matrix translation and rotation.
+                            Doesn't make
+                            sense yet.
+                        </p>
+                        <Divider/>
+                        <p>
+                            Made progress on website. Specifically practiced using an animation library and
+                            worked on
+                            fundamental CSS styling.
+                        </p>
+                        <Divider/>
+                        <div>
+                            Made this as fast as I could
+                        </div>
 
 
+                    </Card>
+                </Row>
+                <Col className="row-width-30">
+                    <Card title="Sleep" bordered={true}
+                          style={{
+                              borderRadius: "1.5rem",
+                              flexGrow: 1,
+                              textAlign: "left",
+                              alignContent: "center",
+                              padding: 20
+                          }}>
+                        <div>
+                            Went to bed: TBD
+                        </div>
 
-                        </Card>
-                    </Col>
-                    <Col style={{display: "flex"}}>
-                        <Card title="Sleep" bordered={true}
-                              style={{
-                                  borderRadius: "1.5rem",
-                                  flexGrow: 1,
-                                  textAlign: "left",
-                                  alignContent: "center",
-                                  padding: 20
-                              }}>
-                            <div>
-                                Went to bed: TBD
-                            </div>
-
-                            <div>
-                                Woke up: 2am
-                            </div>
+                        <div>
+                            Woke up: 2am
+                        </div>
 
 
-                            <Divider/>
-                            <div>Energy in morning: Good</div>
-                            <div>Energy in afternoon: Bad</div>
-                        </Card>
-                    </Col>
-                </div>
+                        <Divider/>
+                        <div>Energy in morning: Good</div>
+                        <div>Energy in afternoon: Bad</div>
+                    </Card>
+                </Col>
             </div>);
     }
 }
